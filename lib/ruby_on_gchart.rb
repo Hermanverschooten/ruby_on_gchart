c00a9b9ef3c857234c279ba7c60472cfe815f974
Herman verschooten <Herman@verschooten.net>
2013-06-11 15:55:10 +0200

    Ruby 1.9.3 - Rails 3.2.13

--- lib/ruby_on_gchart.rb
+++ lib/ruby_on_gchart.rb
@@ -1,7 +1,7 @@
 require "GoogleChart/version"
 require 'uri'
 require 'net/http'
-require 'active_support'
+require 'active_support/core_ext'
 require 'GoogleChart/chart'
 require 'GoogleChart/encoding'
 
